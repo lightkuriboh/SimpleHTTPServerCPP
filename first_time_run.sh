78fc244024e8576c6d474f9c2392a31c1236cfbe
kuribohkute <kuribohwithwing@gmail.com>
2021-01-19 18:22:13 +0700

    First time run setup - install g++-9

--- first_time_run.sh
+++ first_time_run.sh
@@ -1,6 +1,9 @@
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt-get install gcc-9
+sudo apt-get install g++-9
 wget https://github.com/Kitware/CMake/releases/download/v3.19.0-rc1/cmake-3.19.0-rc1.tar.gz
 tar -xzvf cmake-3.19.0-rc1.tar.gz
-cd cmake-3.19.0-rc1
+cd cmake-3.19.0-rc1 || exit
 chmod +x bootstrap
 ./bootstrap
 make
