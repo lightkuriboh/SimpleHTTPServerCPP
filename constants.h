4a53344aecc875bbbb47cf0db4f21f09d192c568
kuribohkute <kuribohwithwing@gmail.com>
2021-01-21 18:30:44 +0700

    Add external config

--- constants.h
+++ constants.h
@@ -11,10 +11,7 @@ enum class ReturnStatus {
 };
 
 namespace SimpleHTTPServer {
-    constexpr int maximumPendingConnections = 1000;
-    constexpr int maximumConnections = 20000;
     constexpr int bufferSize = 2048;
-    constexpr int PORT = 2101;
 }
 
 #endif //SIMPLECPPSERVER_CONSTANTS_H
